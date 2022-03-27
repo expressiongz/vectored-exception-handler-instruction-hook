d984ce9b9ad2ac2fc83ffb1a4d2fb15c30300747
expressxen <97218884+expressionLux@users.noreply.github.com>
2022-03-27 13:10:52 +0100

    main.cpp
    main file.

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <windows.h>
+
+auto veh_func(EXCEPTION_POINTERS *p_exception) {
+    if(p_exception->ExceptionRecord->ExceptionCode == STATUS_GUARD_PAGE_VIOLATION) {
+        printf("%s\n", "Triggered page fault. Enabling single stepping mode...");
+        p_exception->ContextRecord->EFlags |= 0x100;
+        return EXCEPTION_CONTINUE_EXECUTION;
+    }
+    if(p_exception->ExceptionRecord->ExceptionCode == STATUS_SINGLE_STEP) {
+        printf("[stepper] Hooked syscall -> %p\n", (void*)p_exception->ExceptionRecord->ExceptionAddress);
+        p_exception->ContextRecord->EFlags |= 0x100;
+        return EXCEPTION_CONTINUE_EXECUTION;
+    }
+    return EXCEPTION_CONTINUE_SEARCH;
+}
+int main() {
+    DWORD old;
+    auto veh_handler = AddVectoredExceptionHandler(1, (PVECTORED_EXCEPTION_HANDLER)veh_func);
+    VirtualProtect((LPVOID)printf, 1, PAGE_EXECUTE_READ | PAGE_GUARD, &old);
+    printf("%s", "enable single stepping.");
+    if(veh_handler == NULL) {
+        printf("%s", "Failed to add VEH.");
+        return 0;
+    }
+    return 0;
+}
