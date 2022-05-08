0706214fe01c1bb5708845aa8fecd4bc015fc8ef
expressxen <97218884+expressionLux@users.noreply.github.com>
2022-05-08 04:10:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,35 +1,37 @@
 #include <iostream>
 #include <windows.h>
 
-auto veh_func(EXCEPTION_POINTERS *p_exception) {
+long __stdcall veh_func(EXCEPTION_POINTERS* p_exception) {
     // check if its a page fault / page guard violation.
-    if(p_exception->ExceptionRecord->ExceptionCode == STATUS_GUARD_PAGE_VIOLATION) {
-        printf("%s\n", "Triggered page fault. Enabling single stepping mode...");
+    if (p_exception->ExceptionRecord->ExceptionCode == STATUS_GUARD_PAGE_VIOLATION) {
+        std::cout << "Triggered page fault. Enabling single stepping mode...\n";
         // enable single stepping mode by bitwise OR'ing the eflags by 256 / 0x100.
         p_exception->ContextRecord->EFlags |= 0x100;
         return EXCEPTION_CONTINUE_EXECUTION;
     }
     // check if  we are single stepping
-    if(p_exception->ExceptionRecord->ExceptionCode == STATUS_SINGLE_STEP) {
+    if (p_exception->ExceptionRecord->ExceptionCode == STATUS_SINGLE_STEP) {
         // print out the address of the current instruction we are at.
-        printf("[stepper] Hooked instruction -> %p\n", reinterpret_cast<void*>(p_exception->ExceptionRecord->ExceptionAddress));
+        std::cout << "[stepper] Hooked instruction -> " << p_exception->ExceptionRecord->ExceptionAddress << "\n";
         p_exception->ContextRecord->EFlags |= 0x100;
         return EXCEPTION_CONTINUE_EXECUTION;
     }
     return EXCEPTION_CONTINUE_SEARCH;
 }
 int main() {
-    DWORD old;
-    PVOID veh;
-    
+    DWORD old; // buffer var for virtualprot
     // add our VEH
-    veh = AddVectoredExceptionHandler(1, reinterpret_cast<PVECTORED_EXCEPTION_HANDLER>(veh_func));
-    if(veh == NULL) { printf("%s", "Failed to get VEH Handle."); return 0; }
-    
-    // enable page guard on printf (this can be with any function. all we need is to cause a page guard, on literally any function and then call that function.) 
-    if(!VirtualProtect(reinterpret_cast<PVOID>(printf), 1, PAGE_EXECUTE_READ | PAGE_GUARD, &old)) { printf("Failed to enable page guard flag with VirtualProtect.\nGLE(get last error) code: %i", GetLastError()); return 0; }
+    const auto* veh = AddVectoredExceptionHandler(1, veh_func);
+    if (veh == NULL) { 
+        std::cout << "Couldn't add vectored exception handler\n";
+        return -1;
+    }
 
-    printf("%s", "This will not output. But the instruction address for this will.\n");
+    // enable page guard on printf (this can be with any function. all we need is to cause a page guard, on literally any function and then call that function.) 
+    if (!VirtualProtect(static_cast<void*>(printf), 1, PAGE_EXECUTE_READWRITE | PAGE_GUARD, &old)) { 
+        std::cout << "Failed to enable page guard flag with VirtualProtect.\nGet last error code:  " << GetLastError() << "\n";
+        return -1;
+    }
     std::cout << "woo" << "\n";
     return 0;
 }
