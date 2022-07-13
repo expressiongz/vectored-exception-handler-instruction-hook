d24245c4a3684b34dfa86d699366f23eaa161e86
expression <97218884+expressiongz@users.noreply.github.com>
2022-07-13 17:06:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,26 @@
 #include <iostream>
 #include <Windows.h>
 
-long __stdcall veh_func(EXCEPTION_POINTERS* p_exception) {
+long __stdcall handler(EXCEPTION_POINTERS* exception) {
 
-    if (p_exception->ExceptionRecord->ExceptionCode == STATUS_GUARD_PAGE_VIOLATION) {
-        std::cout << "[stepper] Triggered page fault. Enabling single stepping mode...\n";
-        p_exception->ContextRecord->EFlags |= 0x100;
+    if (exception->ExceptionRecord->ExceptionCode == STATUS_GUARD_PAGE_VIOLATION) {
+        std::cout << "[stepper] Triggered page fault. Enabled single step\n";
+        exception->ContextRecord->EFlags |= 0x100;
         return EXCEPTION_CONTINUE_EXECUTION;
     }
-    else if (p_exception->ExceptionRecord->ExceptionCode == STATUS_SINGLE_STEP) {
-        std::cout << "[stepper] Hooked instruction -> " << p_exception->ExceptionRecord->ExceptionAddress << "\n";
-        p_exception->ContextRecord->EFlags |= 0x100;
+
+	if (exception->ExceptionRecord->ExceptionCode == STATUS_SINGLE_STEP) {
+        std::printf("[stepper] Hooked instruction -> 0x%p\n", exception->ExceptionRecord->ExceptionAddress);
+        exception->ContextRecord->EFlags |= 0x100;
         return EXCEPTION_CONTINUE_EXECUTION;
     }
     return EXCEPTION_CONTINUE_SEARCH;
 }
+
 int main() {
-    if (const auto* veh = AddVectoredExceptionHandler(true, veh_func); veh == nullptr) {
-        std::cout << "Couldn't add vectored exception handler\n";
-        return -1;
-    }
-    DWORD old;
-    std::cout << "ty kind luxgz\n";
-    VirtualProtect(&printf, 1, PAGE_EXECUTE_READWRITE | PAGE_GUARD, &old);
+    AddVectoredExceptionHandler(true, &handler);
+    DWORD vp_old_protection{ 0 };
+    VirtualProtect(&printf, 1, PAGE_EXECUTE_READ | PAGE_GUARD, &vp_old_protection);
     std::cout << "ty kind virtualprotect";
     return 0;
 }
